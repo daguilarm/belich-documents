8be89f8d1b8166c38196624092a6129df517c301
daguilarm <damian.aguilarm@gmail.com>
2019-12-08 15:00:56 +0100

    Update assets

--- navigation.php
+++ navigation.php
@@ -9,4 +9,19 @@ return [
         ],
     ],
     'Install||Instalación' => '_location_/install',
+    'Config||Configuración' => '_location_/config',
+    'Commands (Artisan)||Comandos (Artisan)' => '_location_/commands',
+    'Routes||Rutas' => '_location_/routes',
+    'Authorization||Autorización' => '_location_/authorize',
+    'Facades||Facades' => [
+        'children' => [
+            'Belich||Belich' => [
+                'children' => [
+                    'Blade||Blade' => '_location_/facades/belich/blade',
+                    'Models||Modelos' => '_location_/facades/belich/model',
+                    'Operations||Operaciones' => '_location_/facades/belich/operations',
+                ]
+            ],
+        ],
+    ],
 ];
--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -1,17 +1,17 @@
-<li class="pl-4">
+<li>
     @if ($url = is_string($item) ? $item : $item->url)
         @php
             $url = str_replace('_location_', $page->locale, $url);
         @endphp
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-500' : '' }} nav-menu__item hover:text-blue-500"
+            class="{{ 'lvl' . $level }} lvl-base {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active' : '' }} hover:text-blue-500"
         >
             {{ $label }}
         </a>
     @else
         {{-- Menu item without URL--}}
-        <p class="nav-menu__item text-gray-600">{{ $label }}</p>
+        <p class="{{ 'lvl' . $level }} lvl-base">{{ $label }}</p>
     @endif
 
     @if (! is_string($item) && $item->children)
