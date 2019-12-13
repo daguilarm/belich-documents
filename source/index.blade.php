3d1759a9e3cbbef551f8ecfb0c48d940aca725ea
daguilarm <damian.aguilarm@gmail.com>
2019-12-13 22:46:09 +0100

    fixed responsive problem

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,12 +1,12 @@
 @extends('_layouts.master')
 
 @section('body')
-<section class="container max-w-6xl mx-auto px-6 my-4">
+<section class="container max-w-6xl mx-auto px-2 sm:px-6 my-4">
     <div class="flex justify-center w-full mb-12">
         <img class="w-3/4 rounded-lg shadow" src="assets/img/thumbnail.jpg">
     </div>
-    <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
-        <div class="flex-1 w-1/2 m-4 bg-white rounded-lg p-6 shadow-lg">
+    <div class="sm:flex lg:mb-8 mx-auto">
+        <div class="w-full sm:m-4 bg-white rounded-lg p-6 shadow-lg">
             <h3 class="text-blue-800">Belich: The dashboard admin for Laravel</h3>
             <ul class="p-2 mx-6 text-gray-600">
                 <li>No JS frameworks, just vanilla JS.</li>
@@ -15,11 +15,11 @@
                 <li>Fully configurable.</li>
                 <li>Php 7.4, Laravel 6.x, Tailwindcss, ChartistJS...</li>
             </ul>
-            <div class="flex justify-end">
+            <div class="flex justify-center">
                 <a href="en/home/about" class="bg-blue-500 hover:bg-blue-700 hover:text-blue-100 text-white font-bold py-2 px-4 rounded">Documents</a>
             </div>
         </div>
-        <div class="flex-2 w-1/2 m-4 bg-white rounded-lg p-6 shadow-lg">
+        <div class="w-full mt-4 sm:m-4 bg-white rounded-lg p-6 shadow-lg">
             <h3 class="text-blue-800">Belich: Panel de administración para Laravel</h3>
             <ul class="p-2 mx-6 text-gray-600">
                 <li>Sin frameworks JS, solo vanilla JS.</li>
@@ -28,7 +28,7 @@
                 <li>Totalmente configurable.</li>
                 <li>Php 7.4, Laravel 6.x, Tailwindcss, ChartistJS...</li>
             </ul>
-            <div class="flex justify-end">
+            <div class="flex justify-center">
                 <a href="es/home/about" class="bg-blue-500 hover:bg-blue-700 hover:text-blue-100 text-white font-bold py-2 px-4 rounded">Documentación</a>
             </div>
         </div>
