fd64cb5e8bad7f9bf9f8205003799551a6e6eb4e
daguilarm <damian.aguilarm@gmail.com>
2019-12-17 12:50:24 +0100

    refactoring images

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -24,7 +24,23 @@
         @endif
 
         <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/images/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/images/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/images/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/images/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/images/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/assets/images/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/assets/images/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
 
         @stack('meta')
 
@@ -45,7 +61,7 @@
             <div class="container max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center justify-between">
                     <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                        <div class="text-lg md:text-2xl text-blue-900 font-semibold hover:text-blue-600 my-0 pr-4"><img src="../../../../assets/img/logo.svg" alt="Belich" class="bg-transparent dropdown-click shadow-none float-left"></div>
+                        <div class="text-lg md:text-2xl text-blue-900 font-semibold hover:text-blue-600 my-0 pr-4"><img src="../../../../assets/images/logo/logo.svg" alt="Belich" class="bg-transparent dropdown-click shadow-none float-left"></div>
                     </a>
                     <div class="flex w-auto">
                         <div class="dropdown">
